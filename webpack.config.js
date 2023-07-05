a0ac6a7988e92d63cc5f0fce45d0a318c5af34ff
xzthemeofsss <xzthemeofsss@gmail.com>
2023-07-05 16:20:04 +0800

    ✨ feat: Update entry path in webpack.config.js
    Update the entry path in webpack.config.js from 'index.js' to 'src/index.js' to ensure correct file resolution.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   target: 'webworker',
   output: {
     filename: 'worker.js',
