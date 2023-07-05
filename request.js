236d0c2e18d4ac65a7b53f7a8a58b3ebf8805741
xzthemeofsss <xzthemeofsss@gmail.com>
2023-07-05 15:09:59 +0800

    github actions

---
+++ request.js
@@ -0,0 +1,20 @@
+export async function saveUrl(body) {
+  await fetch(CUBOX_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ...body, type: 'url', folder: 'Telegram 收藏' }),
+  });
+}
+
+export async function saveMemo(body) {
+  const result = await fetch(CUBOX_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ...body, type: 'memo' }),
+  });
+  console.log('result :>> ', result);
+}
