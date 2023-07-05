94e47e18655c0edcbdb42504a735d6e2bec986d9
xzthemeofsss <xzthemeofsss@gmail.com>
2023-07-05 15:09:48 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { Telegraf } = require('telegraf');
 const { Application, Router } = require('@cfworker/web');
 const createTelegrafMiddleware = require('cfworker-middleware-telegraf');
+const { saveMemo, saveUrl } = require('./request');
 
-const bot = new Telegraf(self.BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 
-bot.start();
 bot.on('message', async ctx => {
   const { text, caption_entities, caption, entities } = ctx.message;
+  console.log('ctx.message :>> ', ctx.message);
   if (caption_entities) {
     await Promise.all(
       caption_entities.map(async entity => {
@@ -18,27 +19,23 @@ bot.on('message', async ctx => {
         }
       })
     );
-  }
-
-  if (entities) {
+  } else if (entities) {
     await Promise.all(
       entities.map(async entity => {
         if (entity.type === 'text_link' || entity.type === 'url') {
-          const title = msg.text;
+          const title = text;
           const { url } = entity;
           await saveUrl({ content: url, title });
           ctx.telegram.sendMessage(ctx.message.chat.id, '链接保存成功啦');
         }
       })
     );
-  }
-
-  if (text) {
-    await saveMemo({ content: msg.text });
+  } else if (text) {
+    await saveMemo({ content: text });
     ctx.telegram.sendMessage(ctx.message.chat.id, '速记保存成功啦');
   }
 });
 
 const router = new Router();
-router.post(`/${self.SECRET_PATH}`, createTelegrafMiddleware(bot));
+router.post(`/${SECRET_PATH}`, createTelegrafMiddleware(bot));
 new Application().use(router.middleware).listen();
